30ab92e903f82a31dd7a3634081bca26e055b4b8
suarminrais <suarmin.rais@gmail.com>
2024-01-18 04:22:28 +0800

    feat: used OpenAPI schema base

--- test_case/urls.py
+++ test_case/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/v1/auth/', include('user.urls')),
     path('api/v1/company', include('company.urls')),
     path('admin/', admin.site.urls),
